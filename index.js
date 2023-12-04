63f5b6c1565603767578ccfc426acf1fa4ecbfc6
rnahill <rei333lly@gmail.com>
2023-12-04 10:42:51 -0500

    added logoOptions array for inquirer

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const inquirer = require('inquirer');
+
+const logoOptions = [
+    
+    {
+        type: 'input',
+        message: 'Enter up to 3 characters for logo text.',
+        name: 'characters'
+    },
+
+    {
+        type: 'input',
+        message: 'Enter  a color keyword or hexadecial number to pick a color for the logo text.',
+        name: 'textColor'
+    },
+
+    {
+        type: 'input',
+        message: 'Choose a shape from the following options: circle, triangle, square',
+        name: 'shape'
+    },
+
+    {
+        type: 'input',
+        message: 'Enter  a color keyword or hexadecial number to pick a color for the logo shape.',
+        name: 'shapeColor'
+    }
+
+]
